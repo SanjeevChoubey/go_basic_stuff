05e14f3fbbc1fe3d0decb594b313cd3389afb18b
SanjeevChoubey <schoubey80@gmail.com>
2019-07-16 10:49:48 +0530

    corrected

--- shippy/vessel-service/main.go
+++ shippy/vessel-service/main.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	context "golang.org/x/net/context"
+	
 )
 
 
 type Repository interface {
-	FindAvailable(context.Context, *Specification, *Response) error
+	FindAvailable(*pb.Specification) (*pb.Vessel, error)
 }
 
-type VesselRepository{
+type VesselRepository struct{
 	vessels []*pb.Vessel
 }
 
 
 func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error){
 	for _,vessel := range repo.vessels{
-		if spec.
+		
 	}
 }
 
@@ -25,11 +26,11 @@ type Service struct {
 }
 
 func (s *Service) FindAvailable(ctx context.Context, in *pb.Specification, out *pb.Response) error {
-	out.vessel, err := s.repo.FindAvailable(in)
+	vessel, err := s.repo.FindAvailable(in)
 	if err != nil{
 		return err
 	}
-
+	out.veseel = vessel
 	return nil
 }
 
@@ -40,11 +41,15 @@ func main() {
 	repo := &VesselRepository{vessels}
 
 	srv := micro.NewService(
-		micro.Name("shippy.service.vessel")
+		micro.Name("shippy.service.vessel"),
 	)
 
 	srv.Init()
 
-	pb.
+	pb.RegisterVesselServiceHandler(srv.Server(),&service{repo})
+
+	if err := srv.Run(); err != nil{
+		fmt.println(err)
+	}
 
 }
